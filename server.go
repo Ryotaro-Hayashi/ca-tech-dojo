c6d866645a5ee7a69522917bd00dbd911710e6ab
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-06-04 18:46:42 +0900

    Add：net/http パッケージで hello world

--- server.go
+++ server.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"net/http"
+	"fmt"
+	"log"
 )
 
 func main() {
-	http.ListenAndServe(":8080", nil)
+	// "/hello"に対しての処理を設定
+	http.HandleFunc("/hello", helloHandler)
+
+	// 第二引数でhandlerにnilを渡して、DefaultServeMuxを使用
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// "/hello"に対しての処理
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	// w に書き込み
+	fmt.Fprint(w, "hello world!\n")
 }
