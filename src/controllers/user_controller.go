424ea6c49179427b0e79d4f7bed4f6e335a2c369
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-06-08 18:25:38 +0900

    Add：コントローラーからデータベース接続して、処理を追加

--- src/controllers/user_controller.go
+++ src/controllers/user_controller.go
@@ -3,10 +3,25 @@ package controllers
 import (
 	"net/http"
 	"fmt"
+	"ca-tech-dojo/database"
 )
+
+type UserController struct {
+	SqlHandler database.SqlHandler
+}
+
+func NewUserController(sqlHandler database.SqlHandler) UserController {
+	return UserController {
+		SqlHandler: sqlHandler,
+	}
+}
 
 // "/hello"に対しての処理
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	// w に書き込み
 	fmt.Fprint(w, "hello world!\n")
+}
+
+func (controller UserController) GoodnightHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "good night!\n")
 }
--- src/server.go
+++ src/server.go
@@ -4,11 +4,17 @@ import (
 	"net/http"
 	"log"
 	"ca-tech-dojo/controllers"
+	"ca-tech-dojo/database"
 )
 
 func main() {
+	// コントローラーの初期化とデータベースの初期化
+	userController := controllers.NewUserController(database.NewSqlHandler())
+
 	// "/hello"に対しての処理を設定
 	http.HandleFunc("/hello", controllers.HelloHandler)
+
+	http.HandleFunc("/user/create", userController.GoodnightHandler)
 
 	// 第二引数でhandlerにnilを渡して、DefaultServeMuxを使用
 	log.Fatal(http.ListenAndServe(":8080", nil))
