00a4e6abf9fe46c6adc30126e3cdeb106100e207
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-06-11 00:58:12 +0900

    Add：エンドポイントを追加

--- src/server.go
+++ src/server.go
@@ -13,8 +13,8 @@ func main() {
 
 	// "/hello"に対しての処理を設定
 	http.HandleFunc("/hello", controllers.HelloHandler)
-
-	http.HandleFunc("/user/create", userController.GoodnightHandler)
+	http.HandleFunc("/good-night", userController.GoodnightHandler)
+	http.HandleFunc("/users/get", userController.Index)
 
 	// 第二引数でhandlerにnilを渡して、DefaultServeMuxを使用
 	log.Fatal(http.ListenAndServe(":8080", nil))
